6a11dcb4ddbcc7ffd088d7233cfa5bfc664b16e8
Iven Li <739750084@qq.com>
2023-06-04 15:51:32 +0800

    在保护模式进行内存读写,并在执行完后返回实模式
    增加调试手段DEBUG32

--- Makefile
+++ Makefile
@@ -1,8 +1,24 @@
-run :
-	gzip -cd a.img.gz > a.img
-	nasm code/boot.asm -o boot.bin
-	dd if=boot.bin of=a.img bs=512 count=1 conv=notrunc
+SRC:=boot.asm
+BIN:=$(subst .asm,.com,$(SRC))
+
+.PHONY : run
+run : mount
 	bochs -q
 
+mount : $(BIN)
+	sudo mkdir /mnt/floppy/
+	sudo mount -o loop a.img /mnt/floppy/
+	sudo cp $(BIN) /mnt/floppy/ -v
+	sudo cp DEBUG32.EXE /mnt/floppy/ -v
+	sudo umount /mnt/floppy/
+	sudo rm -rf /mnt/floppy/
+
+$(BIN) : image
+	nasm code/boot.asm -o boot.com
+
+image :
+	gzip -cd a.img.gz > a.img
+	gzip -cd freedos.img.gz > freedos.img
+
 clean :
-	rm -f a.img boot.bin
+	rm -f *.img *.bin *.com
--- code/boot.asm
+++ code/boot.asm
@@ -1,63 +1,130 @@
 ; ==========================================
-; 编译方法：nasm boot.asm -o boot.bin
+; 编译方法：nasm code/boot.asm -o boot.com
 
 ; 注意80386 cpu采用小端字节序！
 ; ==========================================
-%include	"GDTHead.inc"	; 常量, 宏, 以及一些说明
+%include	"code/GDTHead.inc"	; 常量, 宏, 以及一些说明
+;此处在0100打断点不管用,因为不知道cs的段基地址是多少
+org	0100h				; 在dos系统中,这个地址是空闲可用的
+	xchg bx,bx
+	jmp	LABEL_BEGIN
 
-	org	07c00h			; 给定程序起始地址
-		jmp	LABEL_BEGIN				;07c04
-[SECTION .gdt]			;GDT 全局描述符段
-;0x7c04
-;                              段基址,       段界限     , 属性
-LABEL_GDT:	   Descriptor       0,                0, 0           ; 空描述符 ;07c0c
-;0x7c0c
-LABEL_DESC_CODE32: Descriptor       0, SegCode32Len - 1, DA_C + DA_32; 非一致代码段
-;0x0714
-LABEL_DESC_VIDEO:  Descriptor 0B8000h,           0ffffh, DA_DRW	     ; 显存段 
+[SECTION .gdt]
+; GDT 如发炮制
+;                            段基址,        段界限 , 属性
+LABEL_GDT:         Descriptor    0,              0, 0         ; 空描述符
+LABEL_DESC_NORMAL: Descriptor    0,         0ffffh, DA_DRW    ; Normal 描述符
+LABEL_DESC_CODE32: Descriptor    0, SegCode32Len-1, DA_C+DA_32; 非一致代码段, 32
+LABEL_DESC_CODE16: Descriptor    0,         0ffffh, DA_C      ; 非一致代码段, 16
+LABEL_DESC_DATA:   Descriptor    0,      DataLen-1, DA_DRW    ; Data
+LABEL_DESC_STACK:  Descriptor    0,     TopOfStack, DA_DRWA+DA_32; Stack, 32 位
+LABEL_DESC_TEST:   Descriptor 0500000h,     0ffffh, DA_DRW
+LABEL_DESC_VIDEO:  Descriptor  0B8000h,     0ffffh, DA_DRW    ; 显存首地址
+; GDT 结束
 
-;GDT相关描述信息
-GdtLen		equ	$ - LABEL_GDT	; GDT长度 不占空间，如同立即数
-
-;0x071c
-;GDT寄存器
-GdtPtr		dw	GdtLen - 1	; GDT界限 24-1			此结构6个字节 48位，和gdtr一样大
+GdtLen		equ	$ - LABEL_GDT	; GDT长度
+GdtPtr		dw	GdtLen - 1	; GDT界限
 		dd	0		; GDT基地址
-;071f
 
-; GDT 选择子 相当与GDT表的index，描述了某个GDT记录向GDT开始的偏移
-SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT			;偏移 8,没有权限变化时，这样就可以了
-SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT			;偏移 16,没有权限变化时，这样就可以了
+; GDT 选择子
+SelectorNormal		equ	LABEL_DESC_NORMAL	- LABEL_GDT
+SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorCode16		equ	LABEL_DESC_CODE16	- LABEL_GDT
+SelectorData		equ	LABEL_DESC_DATA		- LABEL_GDT
+SelectorStack		equ	LABEL_DESC_STACK	- LABEL_GDT
+SelectorTest		equ	LABEL_DESC_TEST		- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+; END of [SECTION .gdt]
+
+[SECTION .data1]	 ; 数据段
+ALIGN	32
+[BITS	32]
+LABEL_DATA:
+SPValueInRealMode	dw	0
+; 字符串
+PMMessage:		db	"In Protect Mode now. ^-^", 0	; 在保护模式中显示
+OffsetPMMessage		equ	PMMessage - $$
+StrTest:		db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+OffsetStrTest		equ	StrTest - $$
+DataLen			equ	$ - LABEL_DATA
+; END of [SECTION .data1]
+
+
+; 全局堆栈段
+[SECTION .gs]
+ALIGN	32
+[BITS	32]
+LABEL_STACK:				;因为有这么大的一个栈段,导致无法像以前一样放到引导扇区调试了
+	times 512 db 0
+
+TopOfStack	equ	$ - LABEL_STACK - 1
+
+; END of [SECTION .gs]
 
 
 [SECTION .s16]
 [BITS	16]
 LABEL_BEGIN:
-	mov	ax, cs			;清零ds es ，cs已经为0
+	;清零,设置栈地地址
+	mov	ax, cs
 	mov	ds, ax
 	mov	es, ax
-	mov	ss, ax			;清零，初始化堆栈
-	mov	sp, 0100h			;堆栈寄存器赋值，这个时候用print-stack将看到堆栈头在0x0100
-	; 初始化 32 位代码段描述符 LABEL_DESC_CODE32 的段基地址
+	mov	ss, ax
+	mov	sp, 0100h
+
+	mov	[LABEL_GO_BACK_TO_REAL+3], ax		;ax此时就是cs,给jmp的基地址赋正确的值
+	mov	[SPValueInRealMode], sp				;存储实模式段栈指针,方便回到实模式时恢复
+
+	; 初始化 16 位代码段描述符 ,把LABEL_SEG_CODE16写入段基址
+	mov	ax, cs
+	movzx	eax, ax
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE16
+	mov	word [LABEL_DESC_CODE16 + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE16 + 4], al
+	mov	byte [LABEL_DESC_CODE16 + 7], ah
+
+	; 初始化 32 位代码段描述符 ,把LABEL_SEG_CODE32写入段基址
 	xor	eax, eax
 	mov	ax, cs
 	shl	eax, 4
-	add	eax, LABEL_SEG_CODE32		;放入段基地址
+	add	eax, LABEL_SEG_CODE32
 	mov	word [LABEL_DESC_CODE32 + 2], ax
 	shr	eax, 16
 	mov	byte [LABEL_DESC_CODE32 + 4], al
-;7c48
 	mov	byte [LABEL_DESC_CODE32 + 7], ah
 
-;7c4c
-	; 维护GdtPtr
+	; 初始化数据段描述符,把LABEL_DESC_DATA写入段基址
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_DATA
+	mov	word [LABEL_DESC_DATA + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_DATA + 4], al
+	mov	byte [LABEL_DESC_DATA + 7], ah
+
+	; 初始化堆栈段描述符,把LABEL_DESC_STACK写入段基址
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_STACK
+	mov	word [LABEL_DESC_STACK + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_STACK + 4], al
+	mov	byte [LABEL_DESC_STACK + 7], ah
+
+	; 维护好GdtPtr, 为加载 GDTR 作准备
 	xor	eax, eax
 	mov	ax, ds
 	shl	eax, 4
 	add	eax, LABEL_GDT		; eax <- gdt 基地址
 	mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
-	; 加载 GdtPtr到GDTR
-	lgdt	[GdtPtr]		;gdtr:base=0x0000000000007c04, limit=0x17
+
+	; 加载 GDTR
+	lgdt	[GdtPtr]
+
 	; 关中断
 	cli
 
@@ -66,29 +133,206 @@ LABEL_BEGIN:
 	or	al, 00000010b
 	out	92h, al
 
-	; 控制寄存器cr0的0位置1，CPU调整到保护模式
+	; 准备切换到保护模式
 	mov	eax, cr0
 	or	eax, 1
 	mov	cr0, eax
 
-	; 切换cs：ip开始执行保护模式32位代码
-	jmp	dword SelectorCode32:0
+	; 利用32位代码段的选择子进行跳转,真正进入保护模式
+	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs, 并跳转到 Code32Selector:0  处
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ss, ax
+
+	mov	sp, [SPValueInRealMode]		;回复栈指针
+
+	in	al, 92h		; `.
+	and	al, 11111101b	;  | 关闭 A20 地址线
+	out	92h, al		; /
+
+	sti			; 开中断
+
+	mov	ax, 4c00h	; `.
+	int	21h		; /  回到 DOS
+; END of [SECTION .s16]
+
 
 [SECTION .s32]; 32 位代码段. 由实模式跳入.
 [BITS	32]
-;0x7c80
+
 LABEL_SEG_CODE32:
+	;在32位保护模式下,就要用选择子寻址啦!
+	mov	ax, SelectorData
+	mov	ds, ax			; 数据段选择子
+	mov	ax, SelectorTest
+	mov	es, ax			; 测试段选择子
 	mov	ax, SelectorVideo
 	mov	gs, ax			; 视频段选择子
 
-	mov	edi, (80 * 11 + 79) * 2	; 屏幕第 11 行, 第 79 列。
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
-	mov	al, 'P'
-	;32位保护模式使用 选择子：偏移 的方式进行寻址
-	;此处直接向显存写入P
+	mov	ax, SelectorStack
+	mov	ss, ax			; 堆栈段选择子
+
+	mov	esp, TopOfStack ; 栈指针偏移指向栈顶
+
+
+	; 下面显示一个字符串 "In Protect Mode now. ^-^"
+	mov	ah, 0Dh			; 0000: 黑底    1101: 洋红
+	xor	esi, esi		;清零
+	xor	edi, edi		;清零
+	mov	esi, OffsetPMMessage	; 显示多少个字符
+	mov	edi, (80 * 10 + 0) * 2	; 目的数据偏移。屏幕第 10 行, 第 0 列。
+	;cld相对应的指令是std，二者均是用来操作方向标志位DF（Direction Flag）。
+	;cld使DF 复位，即是让DF=0，std使DF置位，即DF=1.
+	;通过执行cld或std指令可以控制方向标志DF，决定内存地址是增大（DF=0，向高地址增加）还是减小（DF=1，向地地址减小）。
+	cld	
+.1:	;不断循环,将esi地址段的内容放入显存,直到esi全0(一次两个Byte)
+	lodsb
+	test	al, al
+	jz	.2
 	mov	[gs:edi], ax
+	add	edi, 2
+	jmp	.1
+.2:	; 显示完毕
+
+	call	DispReturn
+	;读内存,展示,回车
+	call	TestRead
+	;写内存
+	call	TestWrite
+	;读内存,展示,回车
+	call	TestRead
 
 	; 到此停止
-	jmp	$
-SegCode32Len	equ	$ - LABEL_SEG_CODE32	;值15
-;没有0xaa55因为我们复用了之前的软盘镜像a.img，这个镜像已经被做成了可引导的。
+	jmp	SelectorCode16:0
+
+; ------------------------------------------------------------------------
+TestRead:
+	xor	esi, esi
+	mov	ecx, 8
+.loop:
+	mov	al, [es:esi]
+	call	DispAL
+	inc	esi
+	loop	.loop
+
+	call	DispReturn
+
+	ret
+; TestRead 结束-----------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+TestWrite:
+	push	esi
+	push	edi
+	xor	esi, esi
+	xor	edi, edi
+	mov	esi, OffsetStrTest	; 源数据偏移
+	cld
+.1:	;不断循环,将esi地址段的字符放入目标内存,直到esi全0(一次一个Byte)
+	lodsb	;从esi 指向的源地址中逐一读取一个Byte,送入AL 中,esi++
+	test	al, al	;两个操作数的对应位之间进行 AND 操作，并根据运算结果设置符号标志位、零标志位和奇偶标志位
+	jz	.2
+	mov	[es:edi], al
+	inc	edi
+	jmp	.1
+.2:
+
+	pop	edi
+	pop	esi
+
+	ret
+; TestWrite 结束----------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; 显示 AL 中的数字
+; 默认地:
+;	数字已经存在 AL 中
+;	edi 始终指向要显示的下一个字符的位置
+; 被改变的寄存器:
+;	ax, edi
+; ------------------------------------------------------------------------
+DispAL:
+	push	ecx
+	push	edx
+
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	dl, al
+	shr	al, 4
+	mov	ecx, 2
+.begin:
+	and	al, 01111b
+	cmp	al, 9
+	ja	.1
+	add	al, '0'
+	jmp	.2
+.1:
+	sub	al, 0Ah
+	add	al, 'A'
+.2:
+	mov	[gs:edi], ax
+	add	edi, 2
+
+	mov	al, dl
+	loop	.begin
+	add	edi, 2
+
+	pop	edx
+	pop	ecx
+
+	ret
+; DispAL 结束-------------------------------------------------------------
+
+
+; 此函数用于打出回车段效果--------------------------------------------------------------
+DispReturn:
+	push	eax
+	push	ebx
+	mov	eax, edi
+	mov	bl, 160
+	div	bl
+	and	eax, 0FFh
+	inc	eax
+	mov	bl, 160
+	mul	bl
+	mov	edi, eax
+	pop	ebx
+	pop	eax
+
+	ret
+; DispReturn 结束---------------------------------------------------------
+
+SegCode32Len	equ	$ - LABEL_SEG_CODE32
+; END of [SECTION .s32]
+
+
+; 16 位代码段. 由 32 位代码段跳入, 跳出后到实模式
+[SECTION .s16code]
+ALIGN	32		;32位对齐 16进制代码
+[BITS	16]
+LABEL_SEG_CODE16:
+	; 利用实模式代码段段选择子,跳回实模式:
+	mov	ax, SelectorNormal
+	mov	ds, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	gs, ax
+	mov	ss, ax
+
+	;回退控制寄存器cr0 的0位
+	mov	eax, cr0
+	and	al, 11111110b
+	mov	cr0, eax
+
+LABEL_GO_BACK_TO_REAL:
+	jmp	0:LABEL_REAL_ENTRY	; 段地址会在程序开始处被设置成正确的值
+
+Code16Len	equ	$ - LABEL_SEG_CODE16
+
+; END of [SECTION .s16code]
